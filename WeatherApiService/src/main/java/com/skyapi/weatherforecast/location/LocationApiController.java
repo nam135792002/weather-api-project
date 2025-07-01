0c3d39fc80e412ad70977290212370a9c7ffb969
nam135792002 <phuongnama32112002@gmail.com>
2025-07-01 14:09:43 +0700

    update: api say hello

--- WeatherApiService/src/main/java/com/skyapi/weatherforecast/location/LocationApiController.java
+++ WeatherApiService/src/main/java/com/skyapi/weatherforecast/location/LocationApiController.java
@@ -59,6 +59,6 @@ public class LocationApiController {
 
     @GetMapping("/hello")
     public ResponseEntity<String> sayHello() {
-        return  ResponseEntity.ok("Hi Nguyen Phuong Nam !!!");
+        return  ResponseEntity.ok("Hi Ri Nguyen");
     }
 }
